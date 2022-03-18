6811dcbac39dc2aad6e520230cf0106179b9b26e
yujiecheng <alfarier@163.com>
2022-03-18 08:46:19 +0800

    Signed-off-by: yujiecheng <alfarier@163.com>

--- PointerLearning/Source/function.c
+++ PointerLearning/Source/function.c
@@ -1,34 +1,16 @@
 #include "stdio.h"
 #include <function.h>
 
-typedef struct data
-{
-	int a[a_lenth];
-	int b[b_lenth];
-	int c[c_lenth];
-	int d[d_lenth];
-}array_combine;
-
-array_combine e[test_struct_lenth] = {
-	{1,2,3,4,5,6,7,8},
-	{5, 4, 7, 4, 5, 6, 74},
-	{1, 3, 2, 4, 5, 7, 6, 8,5,4},
-	{567,254,55,44,33,55}
-};
-
-int array_size(int array[])
-{
-	int array_size = 0;
-	array_size = sizeof(array);
-	return array_size;
-}
-
 void array_of_pointer(void)
 {
 	unsigned int i, j;
 	int	size;
-	
-	int* prr[] = { e->a, e->b, e->c, e->d};
+	int a[] = {1,2,3,4,5,6,78,4};
+	int b[] = {145,34,33,6,78,9,5,6};
+	int c[] = { 2,7,5,3,3,12,2,8 };
+	int d[] = {2,5,6,7,8,2,3,5};
+
+	int* prr[] = { a, b, c, d};
 
 	for ( i=0; i<4; i++)
 	{
@@ -38,7 +20,5 @@ void array_of_pointer(void)
 			printf("%d ", prr[i][j]);//prr[i][j] <==> *(*(prr+i)+j)
 		}
 		printf("\n");
-		size = array_size(e->a);
-		printf("%d ", size);
 	}
 }
