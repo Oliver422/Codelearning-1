dba6f1002d8f8d323fc7cd28a3f8286056f408e1
Oliver422 <353569457@qq.com>
2022-03-20 15:03:54 +0800

    打发打发离开家 Signed-off-by: Oliver422 <353569457@qq.com>

--- PointerLearning/PointerLearing.c
+++ PointerLearning/PointerLearing.c
@@ -9,7 +9,7 @@ int main()
 		//pointer_of_array();
 		//print_two_division_array();
 	    //deliver_test();
-	//	two_division_pointer_deliver();
-	Pointer_of_function();
+		two_division_pointer_deliver();
+	//Pointer_of_function();
 		return 0;
 }
